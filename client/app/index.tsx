6267ca24b8482b8245f013e2959b1edd6c9c186d
Sharvil Limaye <lim.sharvil@gmail.com>
2024-08-31 22:49:40 -0400

    Configure Nativewind and basic routes

---
+++ client/app/index.tsx
@@ -0,0 +1,13 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white" >
+      <Text>Scarlet Sniper</Text>
+      <StatusBar />
+      <Link href="/profile" >Go to Profile</Link>
+    </View>
+  );
+}
